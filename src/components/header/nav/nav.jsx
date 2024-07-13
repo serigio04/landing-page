008776b49f2ae913e000ced222d101a3fbb00b5f
Sergio gomar <sergio.gomar04@gmail.com>
2024-07-13 14:41:29 -0600

    Adding nav to header

---
+++ src/components/header/nav/nav.jsx
@@ -0,0 +1,16 @@
+
+
+const Nav = () => {
+    return <div>
+        <nav className="header-nav">
+            <ul className="nav-list">
+                <li className="list-element">Home</li>
+                <li className="list-element">About</li>
+                <li className="list-element">My collection</li>
+                <li className="list-element">Contact</li>
+            </ul>
+        </nav>
+    </div>
+}
+
+export default Nav
